e26824d9d7422db2b5c19434de1de8a465c15688
Théo <theo.nardin@cri-paris.org>
2025-10-14 16:20:43 +0200

    views

--- welearn_database/modules/views_helper.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.exc import InvalidRequestError
-
-
-def readonly(mapper, connection, target):
-    raise InvalidRequestError(
-        f"{mapper.class_.__name__} is Readonly (view), no changes allowed !"
-    )
